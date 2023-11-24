59d7aecc1b407263672c12be0e0e29c992be1d2e
Vinko Milotić <vinkomilotic632@gmail.com>
2023-11-24 17:56:51 +0100

    Starting log in frontend styling
    Added starting styling based on UI/UX style that was given. Still needed few tweaks: smaller logo and put it on top of the form, widen the form and the inputs as well.

--- client/sveltekit-client/src/routes/+page.svelte
+++ client/sveltekit-client/src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '../app.css';
+</script>
+
+<div class="flex justify-center items-center h-screen">
+	<form action="" class="grid grid-cols-1 place-items-center space-y-6 bg-beige rounded-xl w-80">
+		<img class="" src="./src/multimedia/Logo.png" alt="Insight logo" />
+		<h1 class="text-darkblue text-2xl">Insight</h1>
+		<div class="grid grid-cols-1 place-items-start">
+			<label for="">Korisnicko ime:</label>
+			<input type="text" class="bg-bgbrown" />
+		</div>
+		<div class="grid grid-cols-1 place-items-start">
+			<label for="">Lozinka:</label>
+			<input type="password" class="bg-bgbrown" />
+		</div>
+		<button class="bg-darkblue text-beige px-8">Log in</button>
+	</form>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		background-image: linear-gradient(45deg, #e558fd, #cca7e4, #8f71e7);
+	}
+</style>
--- client/sveltekit-client/tailwind.config.js
+++ client/sveltekit-client/tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: [
+    './src/**/*.{svelte, html, js, ts}',
+  ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'beige': '#F8F7F4',
+        'bgbrown': '#efede6',
+        'strongpurple': '#8f71e7',
+        'strongpink': '#e558fd',
+        'darkblue': '#314867',
+      }
+    },
   },
-  plugins: []
-};
+  plugins: [],
+}
 

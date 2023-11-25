6ff1fa0cf90ead219d52661012b7197f0020a8ad
Vinko Milotić <vinkomilotic632@gmail.com>
2023-11-25 23:35:53 +0100

    Updated design
    Tweaks still needed.

--- client/sveltekit-client/src/routes/moj_profil/+page.svelte
+++ client/sveltekit-client/src/routes/moj_profil/+page.svelte
@@ -3,10 +3,34 @@
 </script>
 
 <div class="h-screen grid grid-cols-6">
-	<div class="border-2 border-solid border-black h-full col-span-1 grid-cols-1">
+	<div class="border-2 border-solid border-strongpurple h-full col-span-1 grid-cols-1">
 		<div class="top-0 h-36 w-full place-items-center flex">
 			<img class="w-16 float-left ml-10 mr-3" src="./src/multimedia/Logo.png" alt="" />
 			<h1 class="float-right mr-10 text-3xl">Insight</h1>
+		</div>
+		<div class="grid grid-cols-2 justify-items-center mb-2.5 mt-2.5">
+			<img class="h-10" src="./src/multimedia/home-icon.svg" alt="" />
+			<p class="h-full mr-24 text-l mt-2">Pocetna</p>
+		</div>
+		<div class="grid grid-cols-2 justify-items-center mb-2.5 mt-2.5">
+			<img class="h-10" src="./src/multimedia/library-icon.svg" alt="" />
+			<p class="h-full mr-8 text-l mt-2 w-full">Moji kolegij</p>
+		</div>
+		<div class="grid grid-cols-2 justify-items-center mb-2.5 mt-2.5">
+			<img class="h-12" src="./src/multimedia/my-groups-icon.svg" alt="" />
+			<p class="h-full mr-8 text-l mt-2 w-full">Moje grupe</p>
+		</div>
+		<div class="grid grid-cols-2 justify-items-center mb-2.5 mt-2.5">
+			<img class="h-10" src="./src/multimedia/chat-icon.svg" alt="" />
+			<p class="h-full mr-8 text-l mt-2 w-full">Poruke</p>
+		</div>
+		<div class="grid grid-cols-2 justify-items-center mt-72">
+			<img
+				class="rounded-full w-12 border-2 border-solid border-strongpurple"
+				src="./src/multimedia/anonymous-user.webp"
+				alt=""
+			/>
+			<p class="h-full mr-8 text-l mt-2 w-full">Ime Prezime</p>
 		</div>
 	</div>
 	<div class="border-2 border-solid border-black h-full col-span-3 grid grid-rows-4 relative">
@@ -33,7 +57,7 @@
 			>
 		</div>
 	</div>
-	<div class="border-2 border-solid border-black h-full col-span-2">text</div>
+	<div class="border-2 border-solid border-strongpurple h-full col-span-2">text</div>
 </div>
 
 <style>

b54315e15cc7c57d60f11baf795a53a470efd684
Mastered1738 <vinkomilotic632@gmail.com>
2023-12-30 20:25:22 +0100

    feat: add log in routing

--- client/sveltekit-client/src/routes/+page.svelte
+++ client/sveltekit-client/src/routes/+page.svelte
@@ -1,25 +1,31 @@
 <script>
+	import { goto } from '$app/navigation';
 	import '../app.css';
+
+	function LogIn(){
+		console.log('====================================');
+		console.log('Changing routes');
+		console.log('====================================');
+		window.location.assign('./moj_profil');
+	}
 </script>
 
-<div class="relative flex justify-center items-center h-screen">
+<div class="relative flex items-center justify-center h-screen">
 	<img class="absolute z-10 w-24 top-36" src="./src/multimedia/Logo.png" alt="Insight logo" />
 	<form
 		action=""
-		class="absolute grid grid-cols-1 place-items-center space-y-6 bg-beige rounded-xl w-80 pb-6"
+		class="absolute grid grid-cols-1 pb-6 space-y-6 place-items-center bg-beige rounded-xl w-80"
 	>
-		<h1 class="text-darkblue text-2xl mt-16">Insight</h1>
+		<h1 class="mt-16 text-2xl text-darkblue">Insight</h1>
 		<div class="grid grid-cols-1 place-items-start">
 			<label for="">Korisnicko ime:</label>
-			<input type="text" class="bg-bgbrown px-3 py-1 rounded-xl" />
+			<input type="text" class="px-3 py-1 bg-bgbrown rounded-xl" />
 		</div>
 		<div class="grid grid-cols-1 place-items-start">
 			<label for="">Lozinka:</label>
-			<input type="password" class="bg-bgbrown px-3 py-1 rounded-xl" />
+			<input type="password" class="px-3 py-1 bg-bgbrown rounded-xl" />
 		</div>
-		<button class="bg-darkblue text-beige px-20 py-1 rounded-xl hover:bg-strongpurple"
-			>Log in</button
-		>
+		<button class="px-20 py-1 bg-darkblue text-beige rounded-xl hover:bg-strongpurple" on:click={LogIn} type="submit">Log in</button>
 	</form>
 </div>
 
